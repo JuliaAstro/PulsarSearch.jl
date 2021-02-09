88674b46cc1977ffd3b3bdb49b32e0f24b18a6df
Matteo Bachetti <matteo@matteobachetti.it>
2021-02-09 15:49:15 +0100

    First draft

--- src/psrsearch.jl
+++ src/psrsearch.jl
@@ -1,5 +1,81 @@
-module psrsearch
+__precompile__()
 
+module psrsearch
+using Distributions
+
+
+function z_n_binned(profile::Array{Float64,1}, n)::Float64
+    total = sum(profile)
+    N = length(profile)
+    phase = Array(range(0, stop = N - 1) / N) * 2 * pi
+
+    if total == 0
+        return 0
+    end
+
+    z = 0
+    for k in range(1, stop = n)
+        s = 0
+        for i in range(1, stop = N)
+            s += profile[i] * sin(k * phase[i])
+        end
+        s = s^2
+        c = 0
+        for i in range(1, stop = N)
+            c += profile[i] * cos(k * phase[i])
+        end
+        c = c^2
+        z += c + s
+    end
+    return z * 2 / total
+end
+
+
+function z_n(phases::Array{Float64,1}, n::Int64)::Float64
+    N = length(phases)
+    z = 0
+    twopiphase = 2 * pi * phases
+    for k in range(1, stop = n)
+        s = 0
+        for ph in twopiphase
+            s += sin(k * ph)
+        end
+        s = s^2
+        c = 0
+        for ph in twopiphase
+            c += cos(k * ph)
+        end
+        c = c^2
+        z += c + s
+    end
+    return z * 2 / N
+end
+
+
+function z_n_search(
+    times::Array{Float64,1},
+    n::Int64,
+    fmin::Float64,
+    fmax::Float64;
+    oversample::Float64 = 2,
+)
+    t0 = first(times)
+    t1 = last(times)
+    df = 1 / (t1 - t0) / oversample
+    freqs = [fmin:df:fmax;]
+    N = length(freqs)
+    stats = zeros(N)
+    for i in range(1, stop = N)
+        phases = times * freqs[i]
+        phases -= floor.(phases)
+        stats[i] = z_n(phases, n)
+    end
+    return freqs, stats
+end
+
+export z_n
+export z_n_binned
+export z_n_search
 # Write your package code here.
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,31 @@
 using psrsearch
 using Test
+using Distributions
 
 @testset "psrsearch.jl" begin
-    # Write your tests here.
+    @testset "Basic" begin
+        flatarr = Array(range(1, stop = 10) / 10)
+        @test z_n(flatarr, 2) < 1e-10
+        @test z_n(ones(10), 2) == 40
+    end
+
+    @testset "Binned" begin
+        flatarr = ones(10)
+        @test z_n_binned(flatarr, 2) < 1e-10
+        arr = zeros(10)
+        arr[1] = 10
+        @test z_n(arr, 2) == 40
+    end
+
+    @testset "Search" begin
+        phases = rand(Normal(0.5, 0.1), 1000)
+        pulse_no = rand(Uniform(0, 1000), 1000)
+        pulse_no = floor.(pulse_no)
+        f = 1.123
+        times = sort((phases + pulse_no) / f)
+        freqs, stats = z_n_search(times, 2, 1.0, 1.5, oversample = 4.0)
+        maxind = argmax(stats)
+        @test abs(freqs[maxind] - f) < 1e-3
+    end
+
 end
