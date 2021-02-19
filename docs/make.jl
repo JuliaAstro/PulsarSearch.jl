fa37d2925999ec20a338fcebcb97775a778b2fe5
Mosè Giordano <mose@gnu.org>
2021-02-19 20:34:48 +0000

    Minor changes to documentation setup
    * Package's project shouldn't list `Test` and `Documenter` as dependencies:
      `Test` is already in the `extra` section, and I moved also `Documenter` there
    * the environment of the documentation shouldn't list the package itself, that
      one is automatically installed by [the CI job](https://github.com/JuliaAstro/psrsearch.jl/blob/59205746747b88b5083adad7f6d4b5c1c1c1ebe5/.github/workflows/Documentation.yml#L19)
      which will build the documentation
    * it's also better to specify compatibility of `Documenter.jl`, to prevent
      building of documentation from stopping to work when there is a new breaking
      release of the package
    * updated links in `docs/make.jl`

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,11 @@ using Documenter
 makedocs(;
     modules=[psrsearch],
     authors="Matteo Bachetti <matteo@matteobachetti.it> and contributors",
-    repo="https://github.com/matteobachetti/psrsearch.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/JuliaAstro/psrsearch.jl/blob/{commit}{path}#L{line}",
     sitename="psrsearch.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://matteobachetti.github.io/psrsearch.jl",
+        canonical="https://juliaastro.github.io/psrsearch.jl",
         assets=String[],
     ),
     pages=[
@@ -17,6 +17,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/matteobachetti/psrsearch.jl",
+    repo="github.com/JuliaAstro/psrsearch.jl",
     devbranch = "main"
 )
